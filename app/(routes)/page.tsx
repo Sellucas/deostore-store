5610e15934e36106379c46e54c3c6cf7fadd2ad3
Sellucas <lucassellmachado@gmail.com>
2023-07-13 11:58:13 -0300

    feat: individual product page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,5 @@
 import getBillboard from "@/actions/get-billboard";
-import getProducts from "@/actions/get-prducts";
+import getProducts from "@/actions/get-products";
 import Billboard from "@/components/billboard";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
