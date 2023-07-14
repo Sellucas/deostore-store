9e32ec9446a34ce917f4138df1b72059e3a64931
Sellucas <lucassellmachado@gmail.com>
2023-07-14 20:40:30 -0300

    fix: checkout button disabled

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -37,7 +37,7 @@ const CartPage = () => {
                 ))}
               </ul>
             </div>
-            <Summary/>
+            <Summary />
           </div>
         </div>
       </Container>
